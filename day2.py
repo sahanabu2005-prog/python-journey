26feff096173a2fd98e73edbeec0ade131a78ad1
sahanabu2005-prog <sahanabu2005@gmail.com>
2026-05-25 19:00:00 +0530

    day 2-if else condition

---
+++ day2.py
@@ -0,0 +1,9 @@
+marks=int(input("enter your marks"))
+if marks>=90:
+ print("grade A -excellent")
+elif marks>=80:
+ print("grade B -good")
+elif marks>=70:
+ print("grade C -work hard")
+else:
+ print("grade F -better luck nxt time" ) 
