b9f70aa9e905270cf2208d9447c817e5babd99ae
sahanabu2005-prog <sahanabu2005@gmail.com>
2026-05-25 19:34:34 +0530

    day3 - for and while loops

---
+++ day3.py
@@ -0,0 +1,17 @@
+for i in range(1,6):
+ print(i)\
+ 
+
+
+# Program 2 - Multiplication table
+number = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(number, "x", i, "=", number * i)
+
+#3rd progm
+count=1
+while count<=5:
+   print("count number",count )
+   count=count+1
+   
